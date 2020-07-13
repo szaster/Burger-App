d2608b06404202e53e78be4029962976bde6b4bc
Svitlana Zaster <szaster@protonmail.com>
2020-07-12 23:20:25 -0500

    installed  dependencies, added code for mysql in connection.js file

---
+++ best_burgers.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE best_burgers;
+
+USE best_burgers;
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
