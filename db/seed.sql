56ccb3b89bb759b011667f05ba0198cfcb7f5417
Svitlana Zaster <szaster@protonmail.com>
2020-07-15 18:28:29 -0500

    Edited front page fonts and colors

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,6 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetarian', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Bizon', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak', true);
-
--- TRUNCATE TABLE burgers;
+TRUNCATE TABLE burgers;
 
 SELECT * FROM burgers;
