0c458bceaeb6576deb78336b17b8ccd49bebbf4f
Svitlana Zaster <szaster@protonmail.com>
2020-07-16 00:25:16 -0500

    final clean up

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,12 @@
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetarian', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Bizon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Bizon', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak', true);
-TRUNCATE TABLE burgers;
+
+-- TRUNCATE TABLE burgers;
+
+USE best_burgers;
+
+UPDATE burgers SET devoured = false WHERE ID = 1; 
 
 SELECT * FROM burgers;
