1d268ca8aa8dedb17867995e4d47f3af0140357b
Svitlana Zaster <szaster@protonmail.com>
2020-07-16 01:17:09 -0500

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql2");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "EmnvaznkzN7!",
-  database: "best_burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "EmnvaznkzN7!",
+    database: "best_burgers_db",
+  });
+}
 
 // Make connection.
 connection.connect(function (err) {
--- db/seed.sql
+++ db/seed.sql
@@ -5,7 +5,7 @@ INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak', true)
 
 -- TRUNCATE TABLE burgers;
 
-USE best_burgers;
+USE best_burgers_db;
 
 UPDATE burgers SET devoured = false WHERE ID = 1; 
 
